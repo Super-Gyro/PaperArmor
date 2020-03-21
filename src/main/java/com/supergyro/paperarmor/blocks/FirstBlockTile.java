a550aab15a39e4aaac3e1172c566a5fad881189f
Super-Gyro <48106056+Super-Gyro@users.noreply.github.com>
2020-03-21 13:22:14 -0500

    Tile entity update, item handler

--- src/main/java/com/supergyro/paperarmor/blocks/FirstBlockTile.java
+++ src/main/java/com/supergyro/paperarmor/blocks/FirstBlockTile.java
@@ -1,12 +1,24 @@
 package com.supergyro.paperarmor.blocks;
 
-import net.minecraft.client.renderer.texture.ITickable;
+
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 
 import static com.supergyro.paperarmor.blocks.ModBlocks.FIRSTBLOCK_TILE;
 
 public class FirstBlockTile extends TileEntity implements ITickableTileEntity {
+
+    private ItemStackHandler handler;
 
     public FirstBlockTile() {
             super(FIRSTBLOCK_TILE);
@@ -14,9 +26,36 @@ public class FirstBlockTile extends TileEntity implements ITickableTileEntity {
 
     @Override
     public void tick() {
-        if (world.isRemote) {
-            System.out.println("FirstBlockTile.tick");
+    }
+
+    @Override
+    public void read(CompoundNBT tag) {
+        CompoundNBT invTag = tag.getCompound("inv");
+        getHandler().deserializeNBT(invTag);
+        super.read(tag);
+    }
+
+    @Override
+    public CompoundNBT write(CompoundNBT tag) {
+        CompoundNBT compound = getHandler().serializeNBT();
+        tag.put("inv", compound);
+        return super.write(tag);
+    }
+
+    private ItemStackHandler getHandler(){
+        if (handler == null) {
+            handler = new ItemStackHandler();
         }
+        return handler;
+    }
+
+    @Nonnull
+    @Override
+    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+        if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+            return LazyOptional.of(() -> (T) getHandler());
+        }
+        return super.getCapability(cap, side);
     }
 
 
